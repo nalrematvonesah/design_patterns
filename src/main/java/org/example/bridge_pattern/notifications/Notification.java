ba3a5d1e76b9e469e78eb734aae92d5392a09bac
nalrematvonesah <nalrematvonesah@gmail.com>
2025-10-09 12:29:48 +0500

    completed bridge

--- src/main/java/org/example/bridge_pattern/notifications/Notification.java
+++ src/main/java/org/example/bridge_pattern/notifications/Notification.java
@@ -1,6 +1,5 @@
 package org.example.bridge_pattern.notifications;
 
-import org.example.bridge_pattern.senders.IMessageSender;
 
 public interface Notification {
     void send(String title, String message);
